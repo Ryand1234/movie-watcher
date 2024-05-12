193eb0425fec6b441efd5fea13c3a09424cdc559
Riyan Dhiman <riyandhiman14@gmail.com>
2024-05-12 22:06:34 +0530

    Added base structure

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,23 @@
+import './home.css';
+
+export default function Home() {
+	return (
+		<div className='home-body'>
+			<div className='home-header'>
+				<div className='home-header-left-side'>
+					<p>Something on left</p>
+				</div>
+				<div className='home-header-right-side'>
+					<p>This is search bar</p>
+				</div>
+			</div>
+			<div className='home-carasoul'>
+				<p>This is some sort of carasol</p>
+			</div>
+			<h2>Latest Movies</h2>
+			<div className='movie-list'>
+				<p>Here are some movie list</p>
+			</div>
+		</div>
+	)
+}
